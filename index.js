14af0324806ea0e6509db40fff32063b1d25cb18
Shubham Suryawanshi <66509702+shubhamsuryaanshi101@users.noreply.github.com>
2022-03-20 16:32:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//1. Addition of two nos.
+function sum(a,b)
+{
+    let z = (a + b);
+    console.log("Addition is -",z)
+}
+sum(10,30);
+
+//2. Subtraction of two nos.
+function sub(a,b)
+{
+    let z = (a - b);
+    console.log("Subtraction is -",z)
+}
+sub(20,3);
+
+//3. Multiplication of two nos.
+function mul(a,b)
+{
+    let z = (a * b);
+    console.log("Multiplication is -",z)
+}
+mul(10,30);
+
+//4. Division of two nos.
+function div(a,b)
+{
+    let z = (a / b);
+    console.log("Division is -",z)
+}
+div(88,4);
+
+//5. power of nos.
+function power(a,b)
+{
+    let z = (a + b)**2;
+    console.log("Power of given nos -",z)
+}
+power(10,2);
